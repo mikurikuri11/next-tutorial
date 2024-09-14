d2b0f9f98cbbe1537a16f9b8e7cfce27f3aebc24
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-09-14 16:21:52 +0900

    ✨ 認証ページ作成

---
+++ app/(dashboard)/auth/page.tsx
@@ -0,0 +1,5 @@
+function AuthPage() {
+  return <h1 className="text-3xl">AuthPage</h1>;
+}
+
+export default AuthPage;
--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const url = "https://www.course-api.com/react-tours-project";
@@ -12,7 +13,7 @@ type Tour = {
 
 // request memoization
 const fetchTours = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(url);
   const data: Tour[] = await response.json();
   return data;
@@ -23,13 +24,29 @@ async function TourPage() {
   return (
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
-      {data.map((tour) => {
-        return (
-          <Link key={tour.id} href={`/tours/${tour.id}`} className="hover:text-blue-500">
-            <h2>{tour.name}</h2>
-          </Link>
-        );
-      })}
+      <div className="grid md:grid-cols-2 gap-8">
+        {data.map((tour) => {
+          return (
+            <Link
+              key={tour.id}
+              href={`/tours/${tour.id}`}
+              className="hover:text-blue-500"
+            >
+              <div className="relative h-48 mb-2">
+                <Image
+                  src={tour.image}
+                  alt={tour.name}
+                  fill
+                  sizes="(max-width:769px) 100vw, (max-width:1200px) 50vw, 33vw"
+                  priority
+                  className="object-cover rounded"
+                />
+              </div>
+              <h2>{tour.name}</h2>
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 }
